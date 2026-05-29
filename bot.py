b93009282428fa03e38342d1b7a2a66896268764
Alexander Orion Centomain <orion.centomain@gmail.com>
2026-05-29 12:37:58 -0300

    Corrigido getenv

--- bot.py
+++ bot.py
@@ -3,8 +3,7 @@ from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filte
 import yt_dlp
 import os
 
-TOKEN = os.getenv("8604112617:AAFzOdCVwFRBIgeNN7POscrRaN-HJKpTghU")
-print("TOKEN:", TOKEN)
+TOKEN = os.getenv("TOKEN")
 
 
 async def baixar_video(update: Update, context: ContextTypes.DEFAULT_TYPE):
